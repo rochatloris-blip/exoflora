756c396efceba90e846fe8e3462af94a6b0b976e
Loris Rochat <loris@MacBook-Pro-de-Loris.local>
2026-07-12 19:14:14 +0200

    Switch from Worker+adapter to pure static Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "static",
-  adapter: cloudflare({
-    platformProxy: { enabled: true },
-  }),
 });
